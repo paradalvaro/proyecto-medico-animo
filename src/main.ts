71acba291857320af35414602a897e2f7cc78a4a
Alvaro Parada <paradalvaro@gmail.com>
2023-03-14 00:14:34 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'http://localhost:3000'
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
   });
   app.useGlobalPipes(
     new ValidationPipe({
